fb84baaf608f29bb2335c32d6108d1cc68befac4
Deborah Schmidt <mail@frauzufall.de>
2023-02-11 00:10:12 +0100

    update run_plots notebook to new cellviewer dataset

--- plots/cell.py
+++ plots/cell.py
@@ -1,19 +1,36 @@
-import z5py
-from pathlib import Path
 import os
+from pathlib import Path
+
 import numpy as np
+import pandas
+import z5py
+
 
 class Cell:
-    def __init__(self, name, path, color, linestyle='-'):
-        self.name = name
+    def __init__(self, path, linestyle='-'):
+        self.file = z5py.File(str(path))
+        self.name = Path(path).name.rstrip('.n5')
         self.path = path
-        self.color = color
         self.linestyle = linestyle
 
     def read_volume(self, volume_name):
         if volume_name is None:
             return None
-        f = z5py.File(str(self.path))
-        project_name = Path(self.path).name.rstrip('.n5')
-        name = 'volumes' + os.path.sep + project_name + '_' + volume_name
-        return np.array(f[name])
+        return np.array(self.file[(self.name + '_' + volume_name)])
+
+    def read_volume_from_group(self, group, volume_name):
+        if volume_name is None:
+            return None
+        return np.array(self.file[group][(self.name + '_' + volume_name)])
+
+    def read_individual_table(self, labelmap_name):
+        return self._read_table('analysis' + os.sep + self.name + '_' + labelmap_name + '_individual.csv')
+
+    def read_overall_table(self, labelmap_name):
+        return self._read_table('analysis' + os.sep + self.name + '_' + labelmap_name + '.csv')
+
+    def _read_table(cell, table_name):
+        return pandas.read_table(os.path.join(cell.path, table_name), delimiter=',')
+
+    def read_distance_map(self, name):
+        return self.read_volume_from_group("analysis", name + "_distance_map")
